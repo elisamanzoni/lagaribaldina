2ed9f45b42a2f6302b476f35ec957a1bb2bb79db
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2018-06-04 16:08:25 +0200

    gallery

--- java_gallery.js
+++ java_gallery.js
@@ -1,52 +1,39 @@
-function compare2(){
-  $("#due").addClass("dx");
-$("#due").removeClass("sx");}
+function compare_a_2(){
+  $("#a_2").addClass("dx");
+$("#a_2").removeClass("sx");
+$("#b_2").addClass("dx");
+$("#b_2").removeClass("sx");}
 
-function compare3(){
-  $("#tre").addClass("dx");
-$("#tre").removeClass("sx");}
+function compare_a_3(){
+  $("#a_3").addClass("dx");
+$("#a_3").removeClass("sx");
+$("#b_3").addClass("dx");
+$("#b_3").removeClass("sx");}
 
-function compare4(){
-  $("#quattro").addClass("dx");
-$("#quattro").removeClass("sx");}
+function compare_a_4(){
+  $("#a_4").addClass("dx");
+$("#a_4").removeClass("sx");
+$("#b_4").addClass("dx");
+$("#b_4").removeClass("sx");}
 
-function compare6(){
-  $("#sei").addClass("dx");
-$("#sei").removeClass("sx");}
+function compare_a_5(){
+  $("#a_5").addClass("dx");
+$("#a_5").removeClass("sx");
+$("#b_5").addClass("dx");
+$("#b_5").removeClass("sx");}
 
-function compare7(){
-  $("#sette").addClass("dx");
-$("#sette").removeClass("sx");}
 
-function compare8(){
-  $("#otto").addClass("dx");
-$("#otto").removeClass("sx");}
 
-function compare9(){
-  $("#nove").addClass("dx");
-$("#nove").removeClass("sx");}
-
-function compare10(){
-  $("#dieci").addClass("dx");
-$("#dieci").removeClass("sx");}
 
 
 function compare(){
-  if ($("#nove").hasClass("dx"))
-  compare10();
-  else if ($("#otto").hasClass("dx"))
-  compare9();
-  else if ($("#sette").hasClass("dx"))
-  compare8();
-  else if ($("#sei").hasClass("dx"))
-  compare7();
-  else if ($("#quattro").hasClass("dx"))
-  compare6();
-  else if ($("#tre").hasClass("dx"))
-  compare4();
-  else   if ($("#due").hasClass("dx"))
-  compare3();
-  else compare2();}
+ if ($("#a_4").hasClass("dx"))
+  compare_a_5();
+  else if ($("#a_3").hasClass("dx"))
+  compare_a_4();
+  else   if ($("#a_2").hasClass("dx"))
+  compare_a_3();
+  else compare_a_2();}
 
 $("#freccia_dx").click(compare);
 
@@ -54,55 +41,40 @@ $("#freccia_dx").click(compare);
 
 
 
-function scompare2(){
-  $("#due").addClass("sx");
-  $("#due").removeClass("dx");}
+function scompare_a_2(){
+  $("#a_2").addClass("sx");
+  $("#a_2").removeClass("dx");
+  $("#b_2").addClass("sx");
+  $("#b_2").removeClass("dx");}
 
-  function scompare3(){
-    $("#tre").addClass("sx");
-  $("#tre").removeClass("dx");}
+  function scompare_a_3(){
+    $("#a_3").addClass("sx");
+  $("#a_3").removeClass("dx");
+  $("#b_3").addClass("sx");
+$("#b_3").removeClass("dx");}
 
-  function scompare4(){
-    $("#quattro").addClass("sx");
-  $("#quattro").removeClass("dx");}
+  function scompare_a_4(){
+    $("#a_4").addClass("sx");
+  $("#a_4").removeClass("dx");
+  $("#b_4").addClass("sx");
+$("#b_4").removeClass("dx");}
 
-  function scompare6(){
-    $("#sei").addClass("sx");
-  $("#sei").removeClass("dx");}
+  function scompare_a_5(){
+    $("#a_5").addClass("sx");
+  $("#a_5").removeClass("dx");
+  $("#b_5").addClass("sx");
+$("#b_5").removeClass("dx");}
 
-  function scompare7(){
-    $("#sette").addClass("sx");
-  $("#sette").removeClass("dx");}
-
-  function scompare8(){
-    $("#otto").addClass("sx");
-  $("#otto").removeClass("dx");}
-
-  function scompare9(){
-    $("#nove").addClass("sx");
-  $("#nove").removeClass("dx");}
-
-  function scompare10(){
-    $("#dieci").addClass("sx");
-  $("#dieci").removeClass("dx");}
 
 
     function scompare(){
-      if ($("#dieci").hasClass("dx"))
-      scompare10();
-      else if ($("#nove").hasClass("dx"))
-      scompare9();
-      else if ($("#otto").hasClass("dx"))
-      scompare8();
-      else if ($("#sette").hasClass("dx"))
-      scompare7();
-      else if ($("#sei").hasClass("dx"))
-      scompare6();
-      else if ($("#quattro").hasClass("dx"))
-      scompare4();
-      else  if ($("#tre").hasClass("dx"))
-      scompare3();
-      else scompare2();}
+     if ($("#a_5").hasClass("dx"))
+      scompare_a_5();
+      else if ($("#a_4").hasClass("dx"))
+      scompare_a_4();
+      else  if ($("#a_3").hasClass("dx"))
+      scompare_a_3();
+      else scompare_a_2();}
 
 
 $("#freccia_sx").click(scompare);

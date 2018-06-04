181d481c3b1790259dcae2713133a32887f3f0ef
elisamanzoni <37081924+elisamanzoni@users.noreply.github.com>
2018-06-04 16:34:16 +0200

    dsgxfh

--- java_gallery.js
+++ java_gallery.js
@@ -78,3 +78,93 @@ $("#b_5").removeClass("dx");}
 
 
 $("#freccia_sx").click(scompare);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function compare_c_2(){
+  $("#c_2").addClass("dx");
+$("#c_2").removeClass("sx");
+$("#d_2").addClass("dx");
+$("#d_2").removeClass("sx");}
+
+function compare_c_3(){
+  $("#c_3").addClass("dx");
+$("#c_3").removeClass("sx");
+$("#d_3").addClass("dx");
+$("#d_3").removeClass("sx");}
+
+function compare_c_4(){
+  $("#c_4").addClass("dx");
+$("#c_4").removeClass("sx");
+$("#d_4").addClass("dx");
+$("#d_4").removeClass("sx");}
+
+
+
+
+
+
+
+function compare2(){
+if ($("#c_3").hasClass("dx"))
+  compare_c_4();
+  else   if ($("#c_2").hasClass("dx"))
+  compare_c_3();
+  else compare_c_2();}
+
+$("#freccia_dx2").click(compare2);
+
+
+
+
+
+function scompare_c_2(){
+  $("#c_2").addClass("sx");
+  $("#c_2").removeClass("dx");
+  $("#d_2").addClass("sx");
+  $("#d_2").removeClass("dx");}
+
+  function scompare_c_3(){
+    $("#c_3").addClass("sx");
+  $("#c_3").removeClass("dx");
+  $("#d_3").addClass("sx");
+$("#d_3").removeClass("dx");}
+
+  function scompare_c_4(){
+    $("#c_4").addClass("sx");
+  $("#c_4").removeClass("dx");
+  $("#d_4").addClass("sx");
+$("#d_4").removeClass("dx");}
+
+
+
+
+
+    function scompare2(){
+     if ($("#c_4").hasClass("dx"))
+      scompare_c_4();
+      else  if ($("#c_3").hasClass("dx"))
+      scompare_c_3();
+      else scompare_c_2();}
+
+
+$("#freccia_sx2").click(scompare2);
